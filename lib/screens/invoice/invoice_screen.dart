009cb1ccb458ebda17e070cc769bc16c69327fe1
rs-phonght <89183657+rs-phonght@users.noreply.github.com>
2021-12-27 11:15:08 +0700

    invoice screen

---
+++ lib/screens/invoice/invoice_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InvoiceScreen extends StatefulWidget {
+  const InvoiceScreen({Key? key}) : super(key: key);
+
+  @override
+  _InvoiceScreenState createState() => _InvoiceScreenState();
+}
+
+class _InvoiceScreenState extends State<InvoiceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
